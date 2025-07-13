b3bfcc7b7b1f7f55334b01f14809c3318732c382
Tyler Ho <tylerho6752@gmail.com>
2025-07-13 13:24:36 -0400

    Fix Tailwind pathing to fix UI styling

--- config/frontend/vite.config.ts
+++ config/frontend/vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss')('./config/frontend/tailwind.config.js'),
+        require('autoprefixer'),
+      ],
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
